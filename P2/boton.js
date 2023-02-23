927e1d91be2d39cacf5e7d9af2635ca9fcef2df1
juanjose <jj.arias.2020@alumnos.urjc.es>
2023-02-23 08:49:03 +0000

    botones_java

--- P2/boton.js
+++ P2/boton.js
@@ -1,9 +1,15 @@
 console.log("Ejecutnado JS...");
 
 //-- Obtener el párrafo
-const boton = document.getElementById('boton');
+const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+
 
 //-- Funcion de retrollamada de la pulsación del párrafo
-boton.onclick = () => {
-    console.log("Has pulsado el boton!");
+boton1.onclick = () => {
+    console.log("Has pulsado el boton1!");
+}
+
+boton2.onclick = () => {
+    console.log("Has pulsado el boton2!");
 }
