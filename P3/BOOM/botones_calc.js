e2ade7386c7257160e3c179876feaca84ad5960b
juanjose <jj.arias.2020@alumnos.urjc.es>
2023-03-09 10:35:49 +0100

    BOOM

--- P3/BOOM/botones_calc.js
+++ P3/BOOM/botones_calc.js
@@ -1,31 +1,63 @@
+numero_secreto = [1, 2, 3, 4];
+
 const ESTADO = {
     INIT: 0,
     OP1: 1,
 }
 
 let estado = ESTADO.INIT;
-
+contador = 0;
 function digito(ev)
 {
     if (estado == ESTADO.INIT) {
-
-        display.innerHTML = ev.target.value;
-
+        crono.start();
         estado = ESTADO.OP1;
-    } else {       
-        display.innerHTML += ev.target.value;
-    } 
-    
-}
+    } else {   
+        for (i=0; i<numero_secreto.length;i++){
+            numeros(ev);
+            
+            }
+        }    
+} 
 
-display = document.getElementById("display_nsecreto")
+
+
+display1 = document.getElementById("display_nsecreto1")
+display2 = document.getElementById("display_nsecreto2")
+display3 = document.getElementById("display_nsecreto3")
+display4 = document.getElementById("display_nsecreto4")
+
 digitos = document.getElementsByClassName("digito")
 
 for (let boton of digitos) {
     boton.onclick = digito;
 }
 
-
+function numeros(ev){
+    if (numero_secreto[i]== parseInt(ev.target.value)){
+        console.log(contador)
+        
+        if (i == 0){
+            display1.innerHTML = ev.target.value;
+            contador +=1;
+        }
+        if (i == 1){
+            display2.innerHTML = ev.target.value;
+            contador +=1;
+        }
+        if (i == 2){
+            display3.innerHTML = ev.target.value;
+            contador +=1;
+        }
+        if (i == 3){
+            display4.innerHTML = ev.target.value;
+            contador +=1;
+        }
+        if(contador == 4){
+            crono.stop();
+        }
+    }
+}
 
 // Cronometro
 //-- Elementos de la gui
@@ -47,6 +79,7 @@ const crono = new Crono(gui.display);
 gui.start.onclick = () => {
     console.log("Start!!");
     crono.start();
+    estado = ESTADO.OP1
 }
   
 //-- Detener el cronómetro
@@ -59,6 +92,10 @@ gui.stop.onclick = () => {
 gui.reset.onclick = () => {
     console.log("Reset!");
     crono.reset();
-    display.innerHTML = '****'
+    display1.innerHTML = '*';
+    display2.innerHTML = '*';
+    display3.innerHTML = '*';
+    display4.innerHTML = '*';
+    contador = 0;
     estado = ESTADO.INIT
 }
