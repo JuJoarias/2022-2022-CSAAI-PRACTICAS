90bfb557453563662540b2b1376c5dd0a58f3cd2
juanjose <jj.arias.2020@alumnos.urjc.es>
2023-03-14 10:12:56 +0100

    BOOM

--- P3/BOOM/botones_calc.js
+++ P3/BOOM/botones_calc.js
@@ -1,5 +1,14 @@
-numero_secreto = [1, 2, 3, 4];
+numero_secreto = [];
 
+function num_sec(){
+    for (x=0; x<4;x++){
+        numero = Math.floor(Math.random()*10);
+        numero_secreto.push(numero);
+    }
+}
+
+num_sec();
+console.log('numero secreto',numero_secreto)
 const ESTADO = {
     INIT: 0,
     OP1: 1,
@@ -9,16 +18,20 @@ let estado = ESTADO.INIT;
 
 function digito(ev)
 {
-    if (estado == ESTADO.INIT) {
-        
+    if (estado == ESTADO.INIT) { 
         estado = ESTADO.OP1;
+        digito(ev);
     } else {   
-        crono.start();
-        for (i=0; i<numero_secreto.length;i++){
+        if (numero_secreto.length != 0){
+            crono.start();
+        
             numeros(ev);
+        }else{
+            console.log('Ya has ganado, recarga la pagina o pulse reset')
+        }
+        
             
-            }
-        }    
+    }    
 } 
 
 
@@ -32,33 +45,50 @@ digitos = document.getElementsByClassName("digito")
 for (let boton of digitos) {
     boton.onclick = digito;
 }
-
+var aciertos = [];
 function numeros(ev){
-    if (numero_secreto[i]== parseInt(ev.target.value)){
-        var aciertos = [];
-        console.log(aciertos)
-        if (i == 0){
-            numero_secreto.splice(i,1);
-            display1.innerHTML = ev.target.value;
-            aciertos.push(numero_secreto[i]);
-        }
-        if (i == 1){
-            numero_secreto.splice(i,1);
-            display2.innerHTML = ev.target.value;
-            aciertos.push(numero_secreto[i]);
-        }
-        if (i == 2){
-            numero_secreto.splice(i,1);
-            display3.innerHTML = ev.target.value;
-            aciertos.push(numero_secreto[i]);
-        }
-        if (i == 3){
-            numero_secreto.splice(i,1);
-            display4.innerHTML = ev.target.value;
-            aciertos.push(numero_secreto[i]);
-        }
-        if(aciertos.length == 4){
-            crono.stop();
+    for (i=0; i<numero_secreto.length;i++){
+        if (numero_secreto[i]== parseInt(ev.target.value)){
+            
+            if (i == 0){
+                
+                display1.innerHTML = ev.target.value;
+                aciertos.push(numero_secreto[i]);
+                numero_secreto[i] = 11;
+                
+            }
+            
+            if (i == 1){
+                
+                display2.innerHTML = ev.target.value;
+                aciertos.push(numero_secreto[i]);
+                numero_secreto[i] = 11;
+    
+            }
+
+            if (i == 2){
+                
+                display3.innerHTML = ev.target.value;
+                aciertos.push(numero_secreto[i]);
+                numero_secreto[i] = 11;
+                
+            }
+
+            if (i == 3){
+        
+                display4.innerHTML = ev.target.value;
+                aciertos.push(numero_secreto[i]);
+                
+            }
+            
+            if(aciertos.length == 4){
+                console.log('HAS GANADO!!!')
+                crono.stop();
+                aciertos = [];
+                numero_secreto =[];
+            }
+
+            break;
         }
     }
 }
@@ -81,25 +111,26 @@ const crono = new Crono(gui.display);
 
 //-- Arranque del cronometro
 gui.start.onclick = () => {
-    console.log("Start!!");
+    
     crono.start();
     estado = ESTADO.OP1
 }
   
 //-- Detener el cronómetro
 gui.stop.onclick = () => {
-    console.log("Stop!");
+    
     crono.stop();
 }
 
 //-- Reset del cronómetro
 gui.reset.onclick = () => {
-    console.log("Reset!");
+    
     crono.reset();
     display1.innerHTML = '*';
     display2.innerHTML = '*';
     display3.innerHTML = '*';
     display4.innerHTML = '*';
-    contador = 0;
     estado = ESTADO.INIT
+    num_sec();
+    console.log('numero secreto',numero_secreto)
 }
