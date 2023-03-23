19fc856cadc51a5be799fa39756611002f203597
juanjose <jj.arias.2020@alumnos.urjc.es>
2023-03-23 10:46:08 +0100

    tiro

--- P4/tiro_parabolico/tiro.js
+++ P4/tiro_parabolico/tiro.js
@@ -7,6 +7,7 @@ const velocidad = document.getElementById("velocidad");
 const angulo = document.getElementById("angulo");
 const ang_disp = document.getElementById("angulo_disp");
 const vel_disp = document.getElementById("vel_disp");
+const display = document.getElementById("display");
 
 velocidad.oninput = () => {
     vel_disp.innerHTML = velocidad.value;
@@ -25,27 +26,36 @@ const ctx = canvas.getContext("2d");
 
 //-- Coordenadas del objeto
 let x = 0;
-let y = canvas.height - 20;
+let y = 21;
 
 //-- Velocidades del objeto
+const crono = new Crono(display);
 
 let velx = 0;
 let t = 0;
-let g = 9.8;
+let g = 0;
 let angle = 0;
+let tiempo = false
 
 go.onclick = () => {
-    velx = velocidad.value;
+  crono.start();
+    velx = velocidad.value ;
     angle = angulo.value
-    update(); 
+    g = 9.8;
+    tiempo = true
     console.log(velx)
+    console.log(angle)
 }
 
 reset.onclick = () => {
     x = 0;
-    y = canvas.height - 20;
+    t = 0;
+    g = 0;
+    tiempo = false;
+    y = 21;
     velx = 0;
-    update();
+    crono.stop();
+    crono.reset();
 }
 
 //-- Función principal de animación
@@ -56,22 +66,31 @@ function update()
   //-- Algoritmo de animacion:
   //-- 1) Actualizar posición del  elemento
   //-- (física del movimiento rectilineo uniforme)
+  if (tiempo){
+    console.log('probando')
+    
+    t += 0.04;
+  }
 
    //-- Condición de rebote en extremos verticales del canvas
-   if (x < 0 || x >= (canvas.width - 50) ) {
-    velx = -velx;
+   if (y <= 25 && t != 5 ) {
+    velx = 0;
+    crono.stop();
+    t = 0;
+    tiempo = false;
+    
   }
 
   //-- Actualizar la posición velocidad
   x = x + velx*0.1 * Math.cos(angle * Math.PI / 180) * t;
-  y = y + velx*0.1 * Math.sin(angle * Math.PI / 180) * t - 0.5 * g * t * t;
-  t += 0.1;
+  y = y + velx * Math.sin(angle * Math.PI / 180) * t - 0.5 * g * t * t;
+  
   //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //-- 3) Dibujar los elementos visibles
   ctx.beginPath();
-    ctx.rect(x, y, 20, 20);
+    ctx.rect(x, canvas.height -y, 20, 20);
 
     //-- Dibujar
     ctx.fillStyle = 'red';
